2856aa90d1bc7728cbfe28cfaeeca85d303d7398
pakaheo <k70923@konkuk.ac.kr>
2024-10-11 22:14:40 +0900

    FEAT : 숫자야구 게임 진행 책임을 지닌 객체

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,2 +1,12 @@
-package baseball;public class BaseballGame {
+package baseball;
+
+public class BaseballGame {
+
+    public static void playBaseball() {
+        printStart();
+    }
+
+    private static void printStart() {
+        System.out.println("숫자 야구를 시작합니다");
+    }
 }
