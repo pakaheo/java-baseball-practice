d84178c90fcdecfc0768e92920391716ba6bb923
pakaheo <k70923@konkuk.ac.kr>
2024-10-11 22:15:09 +0900

    FEAT : 서로 다른 숫자 3개를 골라놓을 책임을 지닌 객체

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,2 +1,29 @@
-package baseball;public class Computer {
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+
+    private static final int SIZE = 3;
+    private static final int MIN_BOUND = 1;
+    private static final int MAX_BOUND = 9;
+
+    private static List<Integer> numbers;
+
+    public static List<Integer> pickNumbers() {
+        numbers = new ArrayList<>();
+        while (numbers.size() < SIZE) {
+            addIfNotContainsNumber(Randoms.pickNumberInRange(MIN_BOUND, MAX_BOUND));
+        }
+        return numbers;
+    }
+
+    private static void addIfNotContainsNumber(int number) {
+        if (!numbers.contains(number)) {
+            numbers.add(number);
+        }
+    }
 }
