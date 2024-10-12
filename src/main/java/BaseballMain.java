0145eb86fabe0da096123c1915cde448424acab3
pakaheo <k70923@konkuk.ac.kr>
2024-10-12 22:35:27 +0900

    FEAT : 스트라이크, 볼, 낫싱을 판정하는 객체

--- src/main/java/BaseballMain.java
+++
@@ -1,7 +0,0 @@
-import baseball.BaseballGame;
-
-public class BaseballMain {
-    public static void main(String[] args) {
-        BaseballGame.playBaseball();
-    }
-}
---
+++ src/main/java/baseball/Judge.java
@@ -0,0 +1,59 @@
+package baseball;
+
+import java.math.BigDecimal;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.DoublePredicate;
+import java.util.function.IntPredicate;
+import java.util.function.LongPredicate;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
+public class Judge {
+
+    private final String STRIKE = "스트라이크";
+    private final String BALL = "볼";
+    private final String NOTHING = "낫싱";
+
+    public String decide(List<Integer> computerPick, List<Integer> userAnswer) {
+        int strike = countStrike(computerPick, userAnswer);
+        int ball = countBall(computerPick, userAnswer);
+
+        return getResultMessage(strike, ball);
+    }
+
+    private int countStrike(List<Integer> computerPick, List<Integer> userAnswer) {
+        return (int) IntStream.range(0, computerPick.size())
+                        .filter(i -> isStrike(computerPick.get(i), userAnswer.get(i)))
+                        .count();
+    }
+
+    private int countBall(List<Integer> computerPick, List<Integer> userAnswer) {
+        return (int) IntStream.range(0, computerPick.size())
+                .filter(i -> isBall(computerPick, computerPick.get(i), userAnswer.get(i)))
+                .count();
+    }
+
+    private boolean isStrike(int computer, int user) {
+        return Objects.equals(computer, user);
+    }
+
+    private boolean isBall(List<Integer> computerPick, int computer, int user) {
+        return !isStrike(computer, user) && computerPick.contains(user);
+    }
+
+    private String getResultMessage(int strike, int ball) {
+        if (strike + ball == 0) {
+            return NOTHING;
+        }
+        if (strike > 0 && ball == 0) {
+            return strike + STRIKE;
+        }
+        if (strike == 0 && ball > 0) {
+            return ball + BALL;
+        }
+        return ball + BALL + " " + strike + STRIKE;
+    }
+}
