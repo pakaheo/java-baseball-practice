7c2bd9b6736b12147b4d0841802a3afdc4e215eb
pakaheo <k70923@konkuk.ac.kr>
2024-10-12 22:35:59 +0900

    FEAT : 숫자 야구 게임의 사용자 객체

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,2 +1,53 @@
-package baseball;public class User {
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+
+    private final int LENGTH = 3;
+
+    private List<Integer> answer;
+
+    public List<Integer> submitAnswer(String input) {
+        answer = new ArrayList<>();
+        checkInputLength(input);
+
+        for (String text : splitAnswer(input)) {
+            makeAnswer(parseToInt(text));
+        }
+
+        return answer;
+    }
+
+    private void checkInputLength(String input) {
+        if (input.length() != LENGTH) {
+            throw new IllegalArgumentException("숫자는 세 자리여야 합니다.");
+        }
+    }
+
+    private String[] splitAnswer(String input) {
+        return input.split("");
+    }
+
+    private void makeAnswer(int number) {
+        checkDuplicateNumber(number);
+        answer.add(number);
+    }
+
+    private void checkDuplicateNumber(int number) {
+        if (answer.contains(number)) {
+            throw new IllegalArgumentException("중복 숫자는 입력할 수 없습니다");
+        }
+    }
+
+    private int parseToInt(String text) {
+        int number;
+        try {
+            number = Integer.parseInt(text);
+        } catch(Exception e) {
+            throw new IllegalArgumentException("숫자가 아닌 문자는 입력할 수 없습니다");
+        }
+        return number;
+    }
 }
