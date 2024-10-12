96e65acb43453f1d1e2568a3bf4551a16973bb32
pakaheo <k70923@konkuk.ac.kr>
2024-10-12 22:37:04 +0900

    FEAT : ResultView

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -1,2 +1,18 @@
-package view;public class ResultView {
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class ResultView {
+
+    public static void printJudgeMessage(String result) {
+        System.out.println(result);
+    }
+
+    public static void printEndMessage() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public static void printOutOrRestartMessage() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
 }
