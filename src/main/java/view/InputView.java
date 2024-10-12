6c34fff3f4ebeea0c1579e91c8d956eb89617cd8
pakaheo <k70923@konkuk.ac.kr>
2024-10-12 22:36:48 +0900

    FEAT : InputView

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,2 +1,13 @@
-package view;public class InputView {
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static void printStartMessage() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static void printInputNumberMessage() {
+        System.out.print("숫자를 입력하세요 : ");
+    }
 }
