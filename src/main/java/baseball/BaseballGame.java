da676e61101b757bf656b892bc2b315e2ffc351b
pakaheo <k70923@konkuk.ac.kr>
2024-10-12 22:38:40 +0900

    REFACTOR : 숫자 야구 게임 진행 로직 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,12 +1,36 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import view.InputView;
+import view.ResultView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class BaseballGame {
 
     public static void playBaseball() {
-        printStart();
+        Computer computer = new Computer();
+        User user = new User();
+        Judge judge = new Judge();
+        String result;
+
+        List<Integer> computerPick =  computer.pickNumbers();
+        List<Integer> userAnswer;
+
+        do {
+            InputView.printStartMessage();
+            result = "";
+            while (!Objects.equals(result, "3스트라이크")) {
+                InputView.printInputNumberMessage();
+                userAnswer = user.submitAnswer(Console.readLine());
+                result = judge.decide(computerPick, userAnswer);
+                ResultView.printJudgeMessage(result);
+            }
+            ResultView.printEndMessage();
+            ResultView.printOutOrRestartMessage();
+        } while (!Objects.equals(Console.readLine(), "2"));
     }
 
-    private static void printStart() {
-        System.out.println("숫자 야구를 시작합니다");
-    }
 }
